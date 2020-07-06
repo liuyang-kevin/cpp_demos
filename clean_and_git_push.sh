35cd55001cd28e01c456802cbddb79152e2d7d03
fifthai macbook pro <shadowcat.liuyang@gmail.com>
2020-07-06 10:32:20 +0800

    clean & update

--- clean_and_git_push.sh
+++ clean_and_git_push.sh
@@ -7,6 +7,7 @@ find . -type f -exec file {} \; | grep "PE32" | awk -F ':'  '{print $1}'| sed 's
 # find . -type f -exec file {} \; | grep "PE32" | awk -F ':'  '{print $1}'| xargs rm -rf '{}'
 # find . -type f -exec file {} \; | grep "PE32" | awk -F ':'  '{print $1}'| xargs echo
 
+git pull
 git add .
 git commit -m "clean & update"
 git push origin master
